33c59526ead6a054d556b19a7016cb9aaa025872
Sedricseron <dimon951753@gmail.com>
2021-02-05 21:54:04 +0200

    Chat with Threshold crypto

---
+++ thresh_chat/client_key_params.py
@@ -0,0 +1,17 @@
+import firebase_admin
+from firebase_admin import firestore, credentials
+import json
+
+cred = credentials.Certificate(r'C:\Users\User\Desktop\thresholdcryptochat-firebase-adminsdk-efq0t-9f4616190a.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+user_name = u'dmytro'
+
+read_ref = db.collection(u'key_params')
+read_ref = read_ref.where(u'user', u'==', user_name)
+for doc in read_ref.stream():
+    key_parameters = doc.to_dict()
+
+with open('key_parameters.json', 'w') as json_file:
+    json.dump(key_parameters, json_file)
