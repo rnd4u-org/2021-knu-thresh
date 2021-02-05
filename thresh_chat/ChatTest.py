c1568ecaa48df4ea606a5817324ed75248244d7f
Sedricseron <dimon951753@gmail.com>
2021-02-05 12:25:30 +0200

    Chat without crypto but with norm chatting

--- thresh_chat/ChatTest.py
+++ thresh_chat/ChatTest.py
@@ -1,11 +1,13 @@
+
 import firebase_admin
 from firebase_admin import firestore, credentials
 from datetime import datetime
 import time
 import threading
 import uuid
+
 # ===================== setup =========================
-# user name = u'dimas'
+# user name = u'karina'
 cred = credentials.Certificate(r'C:\Users\User\Desktop\thresholdcryptochat-firebase-adminsdk-efq0t-9f4616190a.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
@@ -16,25 +18,24 @@ def clear_collection(db):
     for d in data.stream():
         d.reference.delete()
 
+clear_collection(db)
 shutdown = False
 messages = []
 
-def receving(user_name):
 
-    while not shutdown:
-        read_ref = db.collection(u'messages')
-        read_ref = read_ref.where(u'user', u'!=', u'dimas')
-        for doc in read_ref.stream():
-            if doc.id not in messages:
-                messages.append(doc.id)
-                print(f"{doc.to_dict()[u'user']} => {doc.to_dict()[u'message'].decode('utf-8')}")
-        time.sleep(10)
+read_ref = db.collection(u'messages')
+callback_done = threading.Event()
+def on_snapshot(collection_snapshot, changes, read_time):
+    for doc in collection_snapshot:
+        if doc.id not in messages and doc.to_dict()[u'user'] != 'dimas':
+            messages.append(doc.id)
+            print(u"{}=>{}".format(doc.to_dict()[u'user'], doc.to_dict()[u'message'].decode("utf-8")))
+    callback_done.set()
 
+query_watch = read_ref.on_snapshot(on_snapshot)
+# rT = threading.Thread(target=receving, args=(u'pashka-cheburashka', ))
+# rT.start()aa
 
-
-
-rT = threading.Thread(target=receving, args=(u'dimas', ))
-rT.start()
 
 while shutdown == False:
     try:
@@ -46,5 +47,3 @@ while shutdown == False:
         #time.sleep(2)
     except:
         shutdown = True
-
-rT.join()
