7d79d4dd60038f74071c55f962fe457f63845896
gullamukola <gullamukola2@gmail.com>
2021-02-08 12:57:24 +0200

    init chat

--- chathint/firestore_chat.py
+++ chathint/firestore_chat.py
@@ -2,13 +2,8 @@
 
 # This script gives a short example of Cloud Firestore - based chat.
 # Read:
-# - Cloud Firebase Python manual at https://firebase.google.com/docs/firestore/quickstart#python
+# - Python manual at https://firebase.google.com/docs/firestore/quickstart#python
 # - chat Android app (to take chat structure): https://www.ericdecanini.com/2019/12/16/instant-chat-messenger-with-cloud-firestore
-
-# ================================================================================
-# 1. read https://firebase.google.com/docs/admin/setup
-# 2. sudo apt install python3-pip
-# 3. sudo pip3 install firebase-admin
 
 import firebase_admin
 from firebase_admin import firestore
@@ -30,14 +25,13 @@ cred = credentials.Certificate('/home/alex/Downloads/chat/threshold-chat-firebas
 firebase_admin.initialize_app(cred) 
 
 # for several chats on one PC:
-# cred = credentials.ApplicationDefault()
 # firebase_admin.initialize_app(cred, {'projectId': project_id,})
 
 db = firestore.client()
 
 # ===================== send message ==========================
 doc_ref = db.collection(u'users').document(u'UserGUID').collection(u'rooms').document(u'RoomID').collection(u'messages').document(str(uuid.uuid4()))
-doc_ref.set({u'text': u'rrrrrrr', u'user': u'uuuuuuu', u'timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")})
+doc_ref.set({u'text': u'Some Text', u'user': u'Some User', u'timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")})
 
 # ===================== read messages =========================
 read_ref = db.collection(u'users').document(u'UserGUID').collection(u'rooms').document(u'RoomID').collection(u'messages')
